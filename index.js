311c038c683e8f7c17791bab758b8896d3b442ae
brobru <brobru@email.ua>
2014-10-07 23:41:42 +0300

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+var io = require('socket.io').listen(3001);
+
+var chat1 = io
+    .of('/chat1')
+    .on('connection', function (socket)
+    {
+        console.log('connection chat1');
+
+        socket.on('disconnect', function ()
+        {
+            console.log('user disconnected chat1');
+        });
+        socket.on('foo_event', function (_data)
+        {
+            console.log(_data);
+        });
+    });
+
+var chat2 = io
+    .of('/chat2')
+    .on('connection', function (socket)
+    {
+        console.log('connection chat2');
+
+        socket.on('disconnect', function ()
+        {
+            console.log('user disconnected chat2');
+        });
+        socket.on('foo_event', function (_data)
+        {
+            console.log(_data);
+        });
+    });
